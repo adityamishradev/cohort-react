124c0453bb0157a19935f97d98a77b0b4dff25e8
Aditya mishra <adityamishra6674.com>
2026-01-21 01:19:55 +0530

    update

--- searchMernProject/backend/routes/itemRoutes.js
+++ searchMernProject/backend/routes/itemRoutes.js
@@ -3,20 +3,44 @@ const Item = require("../models/Item");
 
 const router = express.Router();
 
-/* ADD ITEM */
+/* ================= ADD ITEM ================= */
+
+// POST request jab naya item add karna ho
 router.post("/add", async (req, res) => {
+
+  // req.body se data aata hai
+  // example:
+  // { name: "Phone", category: "Electronics" }
+
   const item = await Item.create(req.body);
+
+  // response me added item bhej diya
   res.json(item);
 });
 
-/* SEARCH ITEM */
+/* ================= SEARCH ITEM ================= */
+
 router.get("/search", async (req, res) => {
+
+  // URL se query nikal rahe hain
+  // /search?q=ph
   const { q } = req.query;
 
+  // MongoDB me search
   const items = await Item.find({
+
+    /*
+      name field me search hoga
+      $regex = partial search
+      q = jo user ne type kiya
+      "i" = case insensitive
+    Jo word tum likho (q), usko name me dhundo — capital ya small letter ki tension mat lo
+    */
+
     name: { $regex: q, $options: "i" }
   });
 
+  // matching items frontend ko bhej do
   res.json(items);
 });
 
