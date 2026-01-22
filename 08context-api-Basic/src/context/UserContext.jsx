825d18136fb8a5cdc1c88df12c0e28bb9d1ba9c5
Aditya mishra <adityamishra6674.com>
2026-01-22 17:27:14 +0530

    folder Update

---
+++ 08context-api-Basic/src/context/UserContext.jsx
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+// Best practice: Context ka naam usually file ke naam jaisa rakha jata hai
+// taaki project readable aur maintainable rahe
+const UserContext = createContext();
+export default UserContext ;
